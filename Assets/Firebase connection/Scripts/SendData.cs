8f1cd49ba42e573b3e5656e1909d6d0d1e899818
Quintijn <quintijn@easysee.nl>
2023-08-02 13:22:18 +0200

    Update
    Export file made

--- Assets/Firebase connection/Scripts/SendData.cs
+++ Assets/Firebase connection/Scripts/SendData.cs
@@ -7,16 +7,31 @@ public class SendData : MonoBehaviour
     [SerializeField]
     private string playerId = "";
 
+    /// <summary>
+    /// Returns a JSON format of the database in the OnPostSuccesfull
+    /// </summary>
+    public void GetDatabaseData()
+    {
+        string path = DatabaseManager.ConstructDatabasePath();
+        DatabaseManager.GetJSON(path, gameObject.name, "OnPostSuccesfull", "OnPostFailed");
+    }
+
+    /// <summary>
+    /// Will increase the number of total clicks of the given button.
+    /// Also increases the total clicks of the button from the given player.
+    /// The results are visable in the Realtime Database of Firebase.
+    /// </summary>
+    /// <param name="button">The clicked button</param>
     public void ButtonPressed(GameObject button)
     {
-        string path = "";
+        string path;
         if (button != null)
         {
             path = DatabaseManager.ConstructDatabasePath("buttons", button.name);
             DatabaseManager.GetAndAdd(path, gameObject.name, "OnPostSuccesfull", "OnPostFailed");
         }
 
-        if (playerId != "")
+        if (!string.IsNullOrEmpty(playerId))
         {
             path = DatabaseManager.ConstructDatabasePath(playerId, "clickedButtons", button.name);
             DatabaseManager.GetAndAdd(path, gameObject.name, "OnPostSuccesfull", "OnPostFailed");
